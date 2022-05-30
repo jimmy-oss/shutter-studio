9bf9195726ecd8f38e98bb0f0942f0f13f7da825
jimmy-oss <jimmynjonge51@gmail.com>
2022-05-30 21:04:39 +0300

    building the database

--- photos/models.py
+++ photos/models.py
@@ -10,9 +10,9 @@ def __str__ (self):
       return self.name
 
 class Photo (models.Model):
-     category = models.ForeignKey(Category,on_delete=models.SET_NULL)
+     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
      image = models.ImageField(null=False, blank=False)
-     description = models.CharField(max_length=500, null=False,blank=False)
+     description = models.TextField()
      
 def __str__ (self):
       return self.description
