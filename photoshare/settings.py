e7dafe4d989ec8a0a6e511ddc21540fd86647943
jimmy-oss <jimmynjonge51@gmail.com>
2022-05-30 21:33:14 +0300

    creating static root directory

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -121,7 +121,7 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
+ 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -129,4 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = BASE_DIR /  'staticfiles'
