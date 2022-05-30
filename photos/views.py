bf27b579a29bbaf177163b64de0213665d2fbab2
jimmy-oss <jimmynjonge51@gmail.com>
2022-05-30 22:39:05 +0300

    adding categories

--- photos/views.py
+++ photos/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from .models  import Category, Photo
 
 # Create your views here.
 def gallery (request):
-       return render(request,'photos/gallery.html')
+       categories = Category.objects.all()
+       photos = Photo.objects.all()
+       context = {'categories':categories,'photos':photos}
+       return render(request,'photos/gallery.html',context)
      
 def viewPhoto (request, pk):
        return render(request,'photos/photo.html')
