3801b3681e37a64cee6c891466e4ce7c6e9963db
jimmy-oss <jimmynjonge51@gmail.com>
2022-05-30 21:21:42 +0300

    making migrations

--- photos/models.py
+++ photos/models.py
@@ -10,7 +10,7 @@ def __str__ (self):
       return self.name
 
 class Photo (models.Model):
-     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True)
      image = models.ImageField(null=False, blank=False)
      description = models.TextField()
      
