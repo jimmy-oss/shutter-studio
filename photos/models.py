0becab2ef8cb1993f944b8c7f22a0dad736641f6
jimmy-oss <jimmynjonge51@gmail.com>
2022-05-30 20:56:15 +0300

    building the database

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,18 @@
+from operator import mod
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+class Category (models.Model):
+     name = models.CharField(max_length=100, null=False,blank=False)
+     
+def __str__ (self):
+      return self.name
+
+class Photo (models.Model):
+     category = models.ForeignKey(Category,on_delete=models.SET_NULL)
+     image = models.ImageField(null=False, blank=False)
+     description = models.CharField(max_length=500, null=False,blank=False)
+     
+def __str__ (self):
+      return self.name
