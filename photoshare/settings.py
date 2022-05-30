6faee2aaaedb8d7bd203e8c069f25f3af6374de2
jimmy-oss <jimmynjonge51@gmail.com>
2022-05-30 22:15:16 +0300

    configuring the root doc

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -128,6 +128,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT = BASE_DIR /  'staticfiles'
