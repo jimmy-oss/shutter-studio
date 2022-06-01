32ea6c7cd3c1a2d15525a88e401f7559a49d38c4
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-01 22:39:12 +0300

    deploying shutterstudio

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+ 
 
 ROOT_URLCONF = 'photoshare.urls'
 
