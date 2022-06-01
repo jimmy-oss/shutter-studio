448b5550823d1c459bdb8ec6dedc7bc41e088669
jimmy-oss <jimmynjonge51@gmail.com>
2022-06-01 20:29:02 +0300

    adding the footer and unnitests

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,19 @@
+from unicodedata import category
 from django.test import TestCase
+from .models import Category,Photo
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.Travel= Category(name = 'Travel')
+# Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Travel,Category))
+ 
+   # Testing Save Method
+    def test_save_method(self):
+        self.Travel.save_gallery()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
